2168c9c1c70732b9e52dfc19896543d7dff5af0a
Marcus Veres <marcus.veres@gmail.com>
2025-08-27 11:22:50 -0400

    installs preact to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-1-good-boy-america.netlify.app"
+  site: "https://astro-tutorial-1-good-boy-america.netlify.app",
+  integrations: [preact()]
 });
